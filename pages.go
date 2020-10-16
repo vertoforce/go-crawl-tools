f53c6fff5e701396ee78b4dd327b734456868dc3
Connor Lake <vertoforce@gmail.com>
2020-10-16 10:21:17 -0700

    Changed CrawlPages to not be multithreaded by default and handles errors

--- pages.go
+++ pages.go
@@ -16,52 +16,64 @@ type PageURLFunc func(page int64) string
 //
 // Each item it finds it sends to itemsChannel.  If you know the type the ParseFunc returns, you can generatea a new channel type casting each item to that type.
 // It will close the channel once it's finished
-func CrawlPages(ctx context.Context, pageURLFunc PageURLFunc, parseFunction ParseFunc, totalPagesFunc TotalPagesFunc, p proxy.Proxy, maxThreads int, itemsChannel chan interface{}) {
+func CrawlPages(ctx context.Context, pageURLFunc PageURLFunc, parseFunction ParseFunc, totalPagesFunc TotalPagesFunc, p proxy.Proxy, maxThreads int, itemsChannel chan interface{}) error {
 	threadLimit := make(chan struct{}, maxThreads)
 	wg := &sync.WaitGroup{}
 
-	go func() {
-		defer close(itemsChannel)
-		defer close(threadLimit)
-		// Crawl first page
-		req, err := http.NewRequestWithContext(ctx, "GET", pageURLFunc(1), nil)
-		if err != nil {
-			return
-		}
-		html, err := CrawlPage(ctx, p, req, itemsChannel, parseFunction)
-		if err != nil {
-			// TODO: Return this somehow
-			return
-		}
-		pageCount := totalPagesFunc(ctx, html)
+	defer close(itemsChannel)
+	defer close(threadLimit)
 
-		// Crawl all other pages
-		for page := int64(2); page <= pageCount; page++ {
-			select {
-			case <-ctx.Done():
-				// Wait for all our child threads to finish before returning
-				// and closing the channel they rely on
-				wg.Wait()
+	// Crawl first page
+	req, err := http.NewRequestWithContext(ctx, "GET", pageURLFunc(1), nil)
+	if err != nil {
+		return err
+	}
+	html, err := CrawlPage(ctx, p, req, itemsChannel, parseFunction)
+	if err != nil {
+		return err
+	}
+	pageCount := totalPagesFunc(ctx, html)
+
+	// Error channel for all async page crawls
+	errors := make(chan error)
+
+	// Crawl all other pages
+	for page := int64(2); page <= pageCount; page++ {
+		select {
+		case <-ctx.Done():
+			// Wait for all our child threads to finish before returning
+			// and closing the channel they rely on
+			wg.Wait()
+			return ctx.Err()
+		case threadLimit <- struct{}{}: // Try to consume a new thread
+		case err := <-errors:
+			// A thread had an error, wait for others to finish and return this error
+			wg.Wait()
+			return err
+		}
+
+		wg.Add(1)
+		go func(currentPage int64) {
+			defer func() { <-threadLimit }() // Mark thread as done
+			defer wg.Done()
+
+			// Build request
+			req, err := http.NewRequestWithContext(ctx, "GET", pageURLFunc(currentPage), nil)
+			if err != nil {
+				errors <- err
 				return
-			case threadLimit <- struct{}{}: // Try to consume a new thread
 			}
 
-			wg.Add(1)
-			go func(currentPage int64) {
-				defer func() { <-threadLimit }() // Mark thread as done
-				defer wg.Done()
+			_, err = CrawlPage(ctx, p, req, itemsChannel, parseFunction)
+			if err != nil {
+				errors <- err
+				return
+			}
+		}(page)
+	}
 
-				// Build request
-				req, err := http.NewRequestWithContext(ctx, "GET", pageURLFunc(currentPage), nil)
-				if err != nil {
-					return
-				}
+	// wait for all threads to be done
+	wg.Wait()
 
-				_, _ = CrawlPage(ctx, p, req, itemsChannel, parseFunction)
-			}(page)
-		}
-
-		// wait for all threads to be done
-		wg.Wait()
-	}()
+	return nil
 }
